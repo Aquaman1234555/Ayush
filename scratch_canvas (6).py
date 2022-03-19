0839197f31200b7cbe6b57ccc9e2151d74224bea
Aquaman1234555 <101315879+Aquaman1234555@users.noreply.github.com>
2022-03-19 09:33:15 +0530

    Add files via upload

---
+++ scratch_canvas (6).py
@@ -0,0 +1,13 @@
+import turtle
+length = 10
+while length < 0:
+    turtle.pencolor('blue')
+    turtle.forward(length)
+    turtle.left(90)
+    break
+    turtle.pencolor('green')
+    turtle.forward(length)
+    turtle.left(90)
+for i in range(0, 10):
+    turtle.right(89)
+    turtle.pencolor('blue')
